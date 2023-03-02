d1cd529a0f8a20bb4461a5b7e15e59f5dc794770
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-02 12:39:45 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt = 0;
+        int idx = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0)
+                cnt++;
+        }
+        int[] answer = new int[cnt];
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer[idx] = i;
+                idx++;
+            }
+        }
+
+        return answer;
+    }
+}
