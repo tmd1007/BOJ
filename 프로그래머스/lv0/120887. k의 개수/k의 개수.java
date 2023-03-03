9d07db703b1f2c31724ba06aade6b05501bce404
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-03 17:46:37 +0900

    [level 0] Title: k의 개수, Time: 2.08 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,15 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
+
         for (int l = i; l <= j; l++) {
-            for (int m = 0; m < String.valueOf(l).length(); m++) {
-                if(String.valueOf(l).substring(m,m+1).equals(String.valueOf(k))){
+            int tmp = l;
+            while(tmp != 0){
+                if(tmp % 10 == k)
                     answer++;
-                }
+                tmp /= 10;
             }
         }
-
 
         return answer;
     }
