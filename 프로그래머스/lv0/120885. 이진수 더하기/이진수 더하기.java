18d681e34adffc1a749b05b76e308fecebd930c7
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-03 19:20:47 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -2,9 +2,7 @@ class Solution {
     public String solution(String bin1, String bin2) {
         String answer = "";
 
-
-
-        answer = Integer.toBinaryString(Integer.parseInt(bin1, 2)+Integer.parseInt(bin2, 2));
+        answer = Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
 
         return answer;
     }
