d1f824c69b69ae4c5859c90c79bd2e87a17d95a7
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-03 19:40:27 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            String str = String.valueOf(array[i]);
+            for (int j = 0; j < str.length(); j++) {
+                if(String.valueOf(str.charAt(j)).equals("7"))
+                    answer++;
+            }
+        }
+
+        return answer;
+    }
+}
