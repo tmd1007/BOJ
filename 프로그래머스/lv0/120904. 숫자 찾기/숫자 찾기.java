cda3ec3aa6061d25954d6d330234148a63b075fc
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-03 12:52:04 +0900

    [level 0] Title: 숫자 찾기, Time: 8.43 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = -1;
-        String str = String.valueOf(num);
-
-        for (int i = 0; i < str.length(); i++) {
-            if(Integer.parseInt(String.valueOf(str.charAt(i))) == k){
-                answer = i + 1;
-                break;
-            }
-        }
+        int answer = ("n" + num).indexOf(String.valueOf(k));
 
         return answer;
     }
