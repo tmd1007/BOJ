02cdea6d412e06dc8c1a9da43dd07680583ac693
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-07 16:12:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.12 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -4,25 +4,14 @@ class Solution {
 
         for (int i = 0; i < babbling.length; i++) {
             babbling[i] = babbling[i].replace("aya", "1");
-            babbling[i] = babbling[i].replace("ye", "2");
-            babbling[i] = babbling[i].replace("woo", "3");
-            babbling[i] = babbling[i].replace("ma", "4");
-            if (babbling[i].matches("\\d+"))
-                if (A(babbling[i]))
-                    answer++;
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty())
+                answer++;
         }
+
         return answer;
     }
-
-    public static boolean A(String arr){
-        for (int i = 0; i < arr.length(); i++) {
-            char c = arr.charAt(i);
-            for (int j = 0; j < arr.length(); j++) {
-                if(i != j && c == arr.charAt(j))
-                    return false;
-            }
-        }
-        return true;
-    }
-
 }
