b8d66b60c743f75295e18e6047d504ee271adbab
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-12 14:58:48 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.06 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
 
 ### 성능 요약
 
-메모리: 64.6 MB, 시간: 0.08 ms
+메모리: 75.4 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,23 +1,20 @@
 class Solution {
     public String solution(String X, String Y) {
-        String answer = "";
         int[] chkX = new int[10];
         int[] chkY = new int[10];
 
-        StringBuilder sb = new StringBuilder(answer);
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < X.length(); i++) {
-            chkX[Integer.parseInt(String.valueOf(X.charAt(i)))]++;
+            chkX[X.charAt(i) - 48]++;
         }
 
         for (int i = 0; i < Y.length(); i++) {
-            chkY[Integer.parseInt(String.valueOf(Y.charAt(i)))]++;
+            chkY[Y.charAt(i) - 48]++;
         }
 
         for (int i = 9; i >= 0; i--) {
-            for (int j = 0; j < Math.min(chkX[i], chkY[i]); j++) {
-                sb.append(i);
-            }
+            sb.append(String.valueOf(i).repeat(Math.min(chkX[i], chkY[i])));
         }
 
         return sb.toString().length() == 0 ? "-1" : sb.toString().indexOf("0") == 0 ? "0" : sb.toString();
