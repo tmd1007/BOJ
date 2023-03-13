472af852e4339647337166782e771280f3593bba
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-13 09:57:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.65 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,13 +4,10 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             answer[i] = Integer.toBinaryString(arr1[i]|arr2[i]);
-            while (answer[i].length() < n){
-                answer[i] = "0" + answer[i];
-            }
+            answer[i] = String.format("%" + n + "s", answer[i]);
             answer[i] = answer[i].replaceAll("1", "#");
             answer[i] = answer[i].replaceAll("0", " ");
         }
-
 
         return answer;
     }
