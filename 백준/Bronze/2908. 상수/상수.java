edefc6dd5a91b59d369d96a7e4bad3031a3181fa
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-15 16:20:51 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -4,16 +4,11 @@ import java.util.StringTokenizer;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String s1 = st.nextToken();
-        String s2 = st.nextToken();
-        StringBuilder sb1 = new StringBuilder(s1);
-        StringBuilder sb2 = new StringBuilder(s2);
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
 
-        int N = Math.max(Integer.parseInt(sb1.reverse().toString()), Integer.parseInt(sb2.reverse().toString()));
-
-        System.out.println(N);
+        System.out.println(Math.max(A, B));
     }
 }
