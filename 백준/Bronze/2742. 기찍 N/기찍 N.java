e690edc3aebad2838be0aa7f48d2321ba1125383
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-15 15:34:39 +0900

    [Bronze IV] Title: 기찍 N, Time: 288 ms, Memory: 20968 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i > 0; i--) {
+            bw.write(i + "\n");
+        }
+
+        bw.close();
+    }
+}
