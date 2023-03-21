9d62b0d706e5f4f23766df761f83f9861e89e628
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-21 13:46:22 +0900

    [level 1] Title: 내적, Time: 1.82 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,11 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] a, int[] b) {
-        int answer = 0;
-
-        for (int i = 0; i < a.length; i++) {
-            answer += a[i] * b[i];
-        }
-        
-        return answer;
+        return IntStream.range(0, a.length).map(n->a[n] * b[n]).sum();
     }
 }
