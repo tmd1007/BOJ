5ce1308f1388c15fe6d428d35edc77ea9e3ebeef
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-23 14:42:01 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
