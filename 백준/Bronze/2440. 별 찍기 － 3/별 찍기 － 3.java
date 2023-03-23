7d16a3f7915d3f366f436e92d9eaaac2d724f855
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-23 15:45:07 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+
+        bw.close();
+    }
+}
