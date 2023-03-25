983bff35ef8d39e4f21c6078538668f4fc43a300
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-25 22:37:32 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            food[i] = food[i] % 2 == 0 ? food[i] : food[i] - 1;
+            for (int j = 0; j < food[i]/2; j++) {
+                sb.append(i);
+            }
+        }
+        StringBuilder sb2 = new StringBuilder(sb);
+        sb2.reverse();
+        return sb.append(0).append(sb2).toString();
+
+    }
+}
