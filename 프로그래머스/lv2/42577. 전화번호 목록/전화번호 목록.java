3ad5bdc3cb2286e74cf1e6b3cb40de8bab657ad0
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-03-31 10:39:46 +0900

    [level 2] Title: 전화번호 목록, Time: 296.22 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phoneBook) {
+        Arrays.sort(phoneBook);
+        for (int i = 0; i < phoneBook.length -1 ; i++) {
+            if(phoneBook[i + 1].startsWith(phoneBook[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
