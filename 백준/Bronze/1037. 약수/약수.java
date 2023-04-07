e6320226b7020cd18a77bd5afa77d1e7960e9cbb
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-07 15:52:21 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
@@ -13,11 +12,17 @@ class Main {
         N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] numArr = new int[N];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < numArr.length; i++) {
             numArr[i] = Integer.parseInt(st.nextToken());
+            if (numArr[i] > max)
+                max = numArr[i];
+            if (numArr[i] < min)
+                min = numArr[i];
         }
 
-        int answer = Arrays.stream(numArr).max().getAsInt() * Arrays.stream(numArr).min().getAsInt();
+        int answer = min * max;
         System.out.println(answer);
     }
 }
