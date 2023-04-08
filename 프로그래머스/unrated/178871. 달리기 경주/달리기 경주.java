c0c16b443bbc77cbb48d626b931692727eaa0806
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-09 01:02:44 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -3,27 +3,20 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = new String[players.length];
         Map<String, Integer> map = new HashMap<>();
-        Map<Integer, String> map2 = new HashMap<>();
         for (int i = 0; i < players.length; i++) {
             map.put(players[i], i);
-            map2.put(i, players[i]);
         }
 
         for (int i = 0; i < callings.length; i++) {
-            String change = map2.get(map.get(callings[i]) -1);
-            map.replace(callings[i], map.get(callings[i]) -1);
-            map.replace(change, map.get(change) + 1);
-            
-            map2.put(map.get(callings[i]), callings[i]);
-            map2.put(map.get(change), change);
+            int rank = map.get(callings[i]);
+            String temp = players[rank-1];
+            players[rank-1] = callings[i];
+            players[rank] = temp;
+            map.put(callings[i], rank-1);
+            map.put(temp, rank);
         }
 
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = map2.get(i);
-        }
-        
-        return answer;
+        return players;
     }
 }
