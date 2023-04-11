64622ca13e540f9b87abf3157fc99561820023f8
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-11 09:13:52 +0900

    [Bronze I] Title: 유진수, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.java
+++ 백준/Bronze/1356. 유진수/유진수.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = sc.nextLine();
+        StringBuilder str = new StringBuilder(br.readLine());
 
         System.out.println(chk(str));
     }
 
-    public static String chk(String str) {
+    public static String chk(StringBuilder str) {
         if (str.length() == 1)
             return "NO";
 
         int idx = 1;
         while(idx != str.length()) {
-            String left = str.substring(0,idx);
-            String right = str.substring(idx);
+            StringBuilder left = new StringBuilder(str.substring(0, idx));
+            StringBuilder right = new StringBuilder(str.substring(idx));
             if (mul(left) == mul(right))
                 return "YES";
             idx++;
@@ -24,7 +26,7 @@ class Main{
         return "NO";
     }
 
-    public static int mul(String s) {
+    public static int mul(StringBuilder s) {
         int result = 1;
         for (int i = 0; i < s.length(); i++) {
             result *= s.charAt(i) - '0';
