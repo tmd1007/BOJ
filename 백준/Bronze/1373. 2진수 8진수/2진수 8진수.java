18f18a2507591bd0795e856596b8c1975e7419a7
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-11 11:29:09 +0900

    [Bronze I] Title: 2진수 8진수, Time: 372 ms, Memory: 38344 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        if (str.length() % 3 == 1) {
+            int n = str.charAt(0) - '0';
+            sb.append(n);
+            str = str.substring(1);
+        }
+
+        if (str.length() % 3 == 2) {
+            int n = ((str.charAt(0) - '0') * 2) + ((str.charAt(1) - '0'));
+            sb.append(n);
+            str = str.substring(2);
+        }
+
+        for (int i = 0; i < str.length(); i = i+3) {
+            String s = str.substring(i, 3+i);
+            int n = ((s.charAt(0) - '0') * 4) + ((s.charAt(1) - '0') * 2) + ((s.charAt(2) - '0'));
+            sb.append(n);
+        }
+
+        System.out.println(sb);
+    }
+}
