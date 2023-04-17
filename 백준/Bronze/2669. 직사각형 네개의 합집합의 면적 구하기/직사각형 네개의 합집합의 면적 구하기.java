52755cfe5100ed2ad7ce20bb74d4bfcd2df64a7e
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-17 09:04:38 +0900

    [Bronze I] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int[][] arr = new int[101][101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int x1, x2, y1, y2;
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
+                    arr[j][k]++;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (arr[i][j] >= 1)
+                    result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
