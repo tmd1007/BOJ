36de9b02432f2b1c683b7694f6905d187d20e23f
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-18 16:28:09 +0900

    [Silver V] Title: 숫자 카드, Time: 884 ms, Memory: 177900 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,36 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        int[] cnt = new int[20000001];
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
 
-        HashSet<Integer> set = new HashSet<>();
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(st.nextToken());
-            set.add(n);
+            cnt[Integer.parseInt(st.nextToken()) + 10000000]++;
+        }
+
+        int idx = 0;
+        for (int i = 0; i < 20000001; i++) {
+            if (cnt[i] > 0) {
+                arr[idx] = i - 10000000;
+                idx++;
+            }
         }
 
         int M = Integer.parseInt(br.readLine());
-        int[] arr = new int[M];
+        int[] search = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int n = Integer.parseInt(st.nextToken());
-            arr[i] = n;
+            search[i] = Integer.parseInt(st.nextToken());
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            if (set.contains(arr[i])) {
-                sb.append(1).append(" ");
+            if (Arrays.binarySearch(arr, search[i]) >= 0) {
+                sb.append(1 + " ");
             } else
-                sb.append(0).append(" ");
+                sb.append(0 + " ");
         }
 
         System.out.println(sb);
