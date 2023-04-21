8abde611eb15cf694042bcbabbdd70e158d235c6
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-21 10:45:43 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1516 ms, Memory: 62944 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    static HashSet<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+            if (set.contains(str[0]))
+                set.remove(str[0]);
+            else
+                set.add(str[0]);
+        }
+
+        ArrayList<String> list = new ArrayList<>(set);
+
+        list.sort(Collections.reverseOrder());
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
