3ecd80fa9a5e7d683850215f4f72bbbd1afa62b4
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-04-23 22:58:35 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        if (a % 2 == 1 && b % 2 == 1) {
+            return a * a + b * b;
+        } else if (a % 2 == 1 || b % 2 == 1) {
+            return (a + b) * 2;
+        } else
+            return Math.abs(a - b);
+    }
+}
