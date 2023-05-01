40d0f20990bf1e5f9d491d441f8a1726acc1a4d2
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-02 08:36:01 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23096 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        for (int n :stack) {
+            sum += n;
+        }
+
+        System.out.println(sum);
+    }
+}
