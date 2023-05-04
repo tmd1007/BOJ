c5cd7f870d4acee974e9b758668825916afe1891
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-04 10:18:17 +0900

    [Silver II] Title: 스택 수열, Time: 364 ms, Memory: 27948 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] target = new int[N];
+
+
+        for (int i = 0; i < N; i++) {
+            target[i] = Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> s = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        int num = 1;
+        int idx = 0;
+        while (num <= N) {
+            s.add(num);
+            sb.append("+").append('\n');
+            num++;
+
+            while (!s.empty() && (s.peek() == target[idx])) {
+                idx++;
+                s.pop();
+                sb.append("-").append('\n');
+            }
+        }
+
+        System.out.println(s.isEmpty() ? sb : "NO");
+    }
+}
