b9428a18424111b43af6a4e3f18c6523aeb2a666
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-09 12:46:42 +0900

    [Bronze I] Title: Meats On The Grill, Time: 116 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) {
+                String str = br.readLine();
+                StringBuilder reverse = new StringBuilder();
+                reverse.append(str).reverse();
+                sb.append(reverse).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
