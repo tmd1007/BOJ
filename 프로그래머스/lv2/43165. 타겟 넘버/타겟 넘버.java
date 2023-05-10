8f20049284eda678d8c4a21d06ae04cf4b490b9b
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-10 17:09:18 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,27 @@
 class Solution {
-
-    int[] numbers;
-    int answer = 0;
-    int target;
-
-    void dfs(int idx, int sum) {
+    
+    private int answer;
+    private int[] numbers;
+    private int target;
+    
+    public void dfs(int idx, int sum) {
         if (idx == numbers.length) {
-            if (sum == target) answer++;
+            if (sum == target) {
+                answer++;
+            }
             return;
         }
         
-        dfs(idx + 1, sum + numbers[idx]);
-        dfs(idx + 1, sum - numbers[idx]);
+        dfs(idx+1, sum + numbers[idx]);
+        dfs(idx+1, sum - numbers[idx]);
     }
-
+    
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-
-        dfs(0, 0);
-
+        
+        dfs(0,0);
+        
         return answer;
     }
 }
