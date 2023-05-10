c5949cdc8ffbf7eaa19c06868504c46bddf58f75
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-10 14:41:09 +0900

    [Silver IV] Title: 피사노 주기, Time: 156 ms, Memory: 16772 KB -BaekjoonHub

--- 백준/Silver/9471. 피사노 주기/피사노 주기.java
+++ 백준/Silver/9471. 피사노 주기/피사노 주기.java
@@ -6,10 +6,15 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 class Main {
+    static int[] dp = new int[500001];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
+        dp[1] = 1;
 
         StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
@@ -17,21 +22,18 @@ class Main {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
             int k = 0;
-            List<Integer> dp = new ArrayList<>();
-            dp.add(0);
-            dp.add(1);
 
             int idx = 2;
             while(true) {
-                dp.add((dp.get(idx-1) + dp.get(idx-2)) % m);
-                if (dp.get(idx) == 0 && dp.get(idx-1) == 1){
+                dp[idx] = (dp[idx - 1] + dp[idx - 2]) % m;
+                if (dp[idx] == 0 && dp[idx-1] == 1) {
                     k = idx;
                     break;
                 }
                 idx++;
             }
 
-            sb.append(n).append(" ").append(k).append('\n');
+            sb.append(n).append(' ').append(k).append('\n');
         }
 
         System.out.println(sb);
