8b5ff76ec2b8c4a00d00c84edadd75bf6e79553a
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-12 18:56:50 +0900

    [Gold V] Title: 토마토, Time: 652 ms, Memory: 120404 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 class Main {
 
     static int[][] graph;
-    static int[][] cnt;
     static int ans;
 
     static int M;
@@ -19,19 +18,9 @@ class Main {
 
     static int green;
 
+    static Queue<int[]> q = new LinkedList<>();
+
     public static void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (graph[i][j] == 1) {
-                    q.add(new int[]{i, j});
-                } else if (graph[i][j] == 0) {
-                    green++;
-                }
-            }
-        }
-
         while(!q.isEmpty()) {
             int qX = q.peek()[0];
             int qY = q.peek()[1];
@@ -41,11 +30,10 @@ class Main {
                 int newY = qY + movY[i];
                 if (newX >= 0 && newX < N && newY >= 0 && newY < M) {
                     if (graph[newX][newY] == 0) {
-                        graph[newX][newY] = 1;
+                        graph[newX][newY] = graph[qX][qY] + 1;
                         green--;
-                        cnt[newX][newY] = cnt[qX][qY] + 1;
                         q.add(new int[]{newX, newY});
-                        ans = Math.max(ans, cnt[newX][newY]);
+                        ans = Math.max(ans, graph[newX][newY]);
                     }
                 }
             }
@@ -60,17 +48,21 @@ class Main {
         N = Integer.parseInt(st.nextToken());
 
         graph = new int[N][M];
-        cnt = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
+                if (graph[i][j] == 1) {
+                    q.add(new int[]{i, j});
+                } else if (graph[i][j] == 0) {
+                    green++;
+                }
             }
         }
 
         bfs();
 
-        System.out.println(green == 0 ? ans : -1);
+        System.out.println(green == 0 ? ans == 0 ? ans : ans - 1 : -1);
     }
 }
