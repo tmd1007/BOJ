c818b5828f1216f7169281a7c617e8f20b8e2c71
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-12 19:23:50 +0900

    [Gold V] Title: 토마토, Time: 644 ms, Memory: 132344 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int M, N, H, green, ans;
+
+    static Queue<int[]> q = new LinkedList<>();
+
+    static int[][][] graph, cnt;
+
+    static int[] movZ = {1,-1,0,0,0,0};
+    static int[] movX = {0,0,1,-1,0,0};
+    static int[] movY = {0,0,0,0,1,-1};
+
+    public static void bfs() {
+        while(!q.isEmpty()) {
+            int qZ = q.peek()[0];
+            int qX = q.peek()[1];
+            int qY = q.peek()[2];
+            q.poll();
+            for (int i = 0; i < 6; i++) {
+                int newZ = qZ + movZ[i];
+                int newX = qX + movX[i];
+                int newY = qY + movY[i];
+                if (newZ >= 0 && newZ < H && newX >= 0 && newX < N && newY >= 0 && newY < M) {
+                    if (graph[newZ][newX][newY] == 0) {
+                        graph[newZ][newX][newY] = 1;
+                        cnt[newZ][newX][newY] = cnt[qZ][qX][qY] + 1;
+                        ans = Math.max(ans, cnt[newZ][newX][newY]);
+                        green--;
+                        q.add(new int[]{newZ, newX, newY});
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        graph = new int[H][N][M];
+        cnt = new int[H][N][M];
+
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < M; k++) {
+                    int n = Integer.parseInt(st.nextToken());
+                    graph[i][j][k] = n;
+                    if (n == 1) {
+                        q.add(new int[]{i, j, k});
+                    } else if (n == 0) {
+                        green++;
+                    }
+                }
+            }
+        }
+
+        bfs();
+
+        System.out.println(green == 0 ? ans : -1);
+    }
+}
