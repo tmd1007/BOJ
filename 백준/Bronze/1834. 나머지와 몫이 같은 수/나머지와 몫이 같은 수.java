c1706b6cb5fd4d332310b8131640e1494bea89d3
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-12 17:33:25 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
+++ 백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -9,19 +8,8 @@ class Main {
         
         int N = Integer.parseInt(br.readLine());
 
-        System.out.println(solution(N));
-    }
+        long sum = (N - 1) * ((long) N * (N + 1)) / 2;
 
-    public static BigInteger solution(int x) {
-        BigInteger sum = BigInteger.valueOf(0);
-        if (x == 1)
-            return sum;
-
-        for (int i = 1; i < x; i++) {
-            long n = ((long) x * i) + i;
-            sum = sum.add(BigInteger.valueOf(n));
-        }
-
-        return sum;
+        System.out.println(sum);
     }
 }
