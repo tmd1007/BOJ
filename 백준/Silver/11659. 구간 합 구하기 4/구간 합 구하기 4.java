25c7c65cb6c14b15cb25fc5f077a599ca20ebbb9
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-15 10:57:23 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 292 ms, Memory: 21208 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+public class Main {
+    
+    static int N,M;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception {
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = read();
+        M = read();
         int[] arr = new int[N+1];
-        st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 1; i <= N; i++) {
-            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        for(int i=1 ;i<=N; i++) {
+            arr[i] = arr[i-1] + read();
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int sum = arr[end] - arr[start-1];
-            sb.append(sum).append('\n');
+        for(int k=0; k<M; k++) {
+            int i = read();
+            int j = read();
+            sb.append(arr[j] - arr[i-1]).append('\n');
         }
 
         System.out.println(sb);
+
+    }
+
+    private static int read() throws Exception{
+        int c, n = System.in.read() & 15;
+
+        while((c=System.in.read()) > 32) {
+            n = (n<<3) + (n<<1) + (c&15);
+        }
+
+        return n;
     }
 }
