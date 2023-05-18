70a392c28573dd28c9f3c8ff62641cedd0d68328
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-18 18:18:34 +0900

    [Silver II] Title: 좌표 압축, Time: 1736 ms, Memory: 165692 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,36 +1,48 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+    static int rank;
+
+    private static int read() throws IOException {
+        boolean isMinus = false;
+        int c , n = System.in.read();
+        if (n == 45) {
+            isMinus = true;
+            n = System.in.read() & 15;
+        } else {
+            n = n & 15;
+        }
+        while((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return isMinus ? -n : n;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
         int[] arr = new int[N];
-        int[] arrSort = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(st.nextToken());
+            int n = read();
             arr[i] = n;
-            arrSort[i] = n;
         }
 
+        int[] arrSort = arr.clone();
         Arrays.sort(arrSort);
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int idx = 0;
         for (int j : arrSort) {
             if (!map.containsKey(j)) {
-                map.put(j, idx);
-                idx++;
+                map.put(j, rank++);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(map.get(arr[i])).append(' ');
+        for (int j : arr) {
+            sb.append(map.get(j)).append(' ');
         }
 
         System.out.println(sb);
