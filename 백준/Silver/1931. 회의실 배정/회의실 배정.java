b266c9425494ffdcb7be465a5f893e87f721c695
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-20 22:02:06 +0900

    [Silver I] Title: 회의실 배정, Time: 508 ms, Memory: 44540 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = start;
+            arr[i][1] = end;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[1];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 0;
+        int recent = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (recent <= arr[i][0]) {
+                recent = arr[i][1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
