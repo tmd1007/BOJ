fa2cd0dfe62a5252568f735c61ba28f1076469bc
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-22 17:15:01 +0900

    [Silver IV] Title: 30, Time: 180 ms, Memory: 16424 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -6,23 +6,28 @@ import java.util.*;
 class Main {
 
     public static String solv(String n) {
-        String[] arr = n.split("");
-        Arrays.sort(arr, Collections.reverseOrder());
+        char[] arr = n.toCharArray();
+        Arrays.sort(arr);
+        char[] sortedArr = new char[arr.length];
 
-        if (!arr[arr.length-1].equals("0")) {
+        if (arr[0] != '0') {
             return "-1";
         }
 
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += Integer.parseInt(arr[i]);
+            sum += Character.getNumericValue(arr[i]);
         }
 
         if (sum % 3 != 0) {
             return "-1";
         }
 
-        return String.join("", arr);
+        for (int i = 0; i < arr.length; i++) {
+            sortedArr[i] = arr[arr.length - 1 - i];
+        }
+
+        return String.valueOf(sortedArr);
     }
 
     public static void main(String[] args) throws IOException {
