5076c9eecb85da2b8a6d3798fc22e0597909bf0a
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-25 09:21:29 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/lv1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT animal_id, name
-from animal_ins
-where intake_condition like 'sick'
-order by animal_id asc;
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION LIKE 'SICK'
+ORDER BY ANIMAL_ID ASC;
