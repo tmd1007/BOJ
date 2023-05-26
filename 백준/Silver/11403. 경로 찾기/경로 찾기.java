e4528e86de743c3a5247b3d6dec9aadcebcb6f47
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-26 18:59:49 +0900

    [Silver I] Title: 경로 찾기, Time: 264 ms, Memory: 30412 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static boolean[][] map;
+    static boolean[] visit;
+    static int N;
+
+    public static boolean bfs(int start, int target) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while(!q.isEmpty()) {
+            int r = q.poll();
+            if (visit[target]) {
+                return true;
+            }
+            for (int i = 0; i < N; i++) {
+                if (map[r][i] && !visit[i]) {
+                    visit[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new boolean[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if (n == 1) {
+                    map[i][j] = true;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                visit = new boolean[N];
+                if (bfs(i, j)) {
+                    sb.append(1).append(' ');
+                } else {
+                    sb.append(0).append(' ');
+                }
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
