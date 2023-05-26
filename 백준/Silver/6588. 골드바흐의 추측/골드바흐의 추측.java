138494e6a0e0512fc3886259e0073c88fd5ee04e
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-05-26 17:37:29 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 680 ms, Memory: 37068 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 class Main {
 
     static boolean[] chk = new boolean[1000001];
-    static ArrayList<Integer> list = new ArrayList<>();
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -24,11 +22,13 @@ class Main {
     }
 
     public static void gold(int n) {
-        for (int i = 0; i < list.size(); i++) {
-            int x = n - list.get(i);
-            if (!chk[x]) {
-                sb.append(n).append(" = ").append(list.get(i)).append(" + ").append(x).append('\n');
-                return;
+        for (int i = 3; i < chk.length; i += 2) {
+            if (!chk[i]) {
+                int x = n - i;
+                if (!chk[x]) {
+                    sb.append(n).append(" = ").append(i).append(" + ").append(x).append('\n');
+                    return;
+                }
             }
         }
 
@@ -38,7 +38,6 @@ class Main {
     public static void chkPrime() {
         for (int i = 3; i * i < chk.length; i += 2) {
             if (!chk[i]) {
-                list.add(i);
                 for (int j = i * i; j < chk.length; j += i) {
                     chk[j] = true;
                 }
