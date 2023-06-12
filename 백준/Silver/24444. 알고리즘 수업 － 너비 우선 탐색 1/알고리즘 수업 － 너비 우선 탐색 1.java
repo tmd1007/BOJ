016e4610cab3f8a279bbed365da21e599d215ae2
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-12 18:52:52 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1040 ms, Memory: 88100 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -1,61 +1,64 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int N, M;
-    static List<List<Integer>> l = new ArrayList<>();
-    static int[] visited;
+class Main {
 
-    public static void main(String[] args) throws IOException{
+    static int N, M, R;
+    static List<ArrayList<Integer>> list = new ArrayList<>();
+    static int[] visit;
+
+    public static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(R);
+        int len = 1;
+        visit[R] = len;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i = 0; i < list.get(cur).size(); i++) {
+                int next = list.get(cur).get(i);
+                if (visit[next] == 0) {
+                    visit[next] = ++len;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int R = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
-        visited = new int[N+1];
-        for(int i = 0; i<=N; i++)
-            l.add(new ArrayList<>());
+        visit = new int[N+1];
 
-        for(int i = 0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-
-            l.get(A).add(B);
-            l.get(B).add(A);
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
         }
 
-        for(int i = 1; i<=N; i++)
-            Collections.sort(l.get(i));
-
-        bfs(R);
-
-        for(int i = 1; i<=N; i++)
-            System.out.println(visited[i]);
-    }
-
-    static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        int cnt = 1;
-
-        q.offer(start);
-        visited[start] = cnt++;
-
-        while(!q.isEmpty()){
-            int a = q.poll();
-
-            for(int i = 0; i<l.get(a).size(); i++){
-                int nextV = l.get(a).get(i);
-
-                if(visited[nextV] != 0)
-                    continue;
-
-                q.offer(nextV);
-                visited[nextV] = cnt++;
-            }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
         }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(list.get(i));
+        }
+
+        bfs();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < visit.length; i++) {
+            sb.append(visit[i]).append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
