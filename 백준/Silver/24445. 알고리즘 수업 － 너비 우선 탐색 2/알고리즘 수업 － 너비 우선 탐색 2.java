871b2f06a4439fd9ce6fa258380752ae91385d57
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-13 22:16:32 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1080 ms, Memory: 90844 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    static List<ArrayList<Integer>> list = new ArrayList<>();
+    static int[] visit;
+
+    public static void bfs(int n) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        int cnt = 0;
+        visit[n] = ++cnt;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i = 0; i < list.get(cur).size(); i++) {
+                int next = list.get(cur).get(i);
+                if (visit[next] == 0) {
+                    visit[next] = ++cnt;
+                    q.add(next);
+                }
+            }
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        visit = new int[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(list.get(i), new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2 - o1;
+                }
+            });
+        }
+
+        bfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(visit[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
