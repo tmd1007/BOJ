f92aa64b84f9fb9f0e4e5a50e26fcb5058c47411
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-15 16:55:42 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static int solv(int[] a, int n) {
+        int i = n / 2;
+        return a[i];
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
