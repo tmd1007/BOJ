4c197f999357d1871facb51f0279fcc553a5cee9
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-16 22:15:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 892 ms, Memory: 92008 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static PriorityQueue<Integer>[] node;
+    static int[] visit;
+    private static int depth;
+
+    public static void dfs(int n) {
+        while(!node[n].isEmpty()) {
+            int next = node[n].poll();
+            if (visit[next] == 0) {
+                visit[next] = ++depth;
+                dfs(next);
+            }
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        node = new PriorityQueue[N+1];
+        visit = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            node[i] = new PriorityQueue<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2 - o1;
+                }
+            });
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            node[u].add(v);
+            node[v].add(u);
+        }
+
+        visit[R] = ++depth;
+        dfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(visit[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
