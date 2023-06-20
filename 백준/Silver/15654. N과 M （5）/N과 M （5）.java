61fe618f8741af1f4c8196f9f78348624dce9a6d
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-20 10:39:17 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 28212 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -22,14 +22,13 @@ class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            if (!visit[data[i]]) {
-                visit[data[i]] = !visit[data[i]];
+            if (!visit[i]) {
+                visit[i] = !visit[i];
                 arr[depth] = data[i];
                 dfs(depth + 1);
-                visit[data[i]] = !visit[data[i]];
+                visit[i] = !visit[i];
             }
         }
-        
     }
     
     public static void main(String[] args) throws IOException {
@@ -39,7 +38,7 @@ class Main {
         M = Integer.parseInt(st.nextToken());
         data = new int[N];
         arr = new int[M];
-        visit = new boolean[10001];
+        visit = new boolean[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(st.nextToken());
