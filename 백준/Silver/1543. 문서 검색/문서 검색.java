f7ee8d72747a8db9d7baacac6b242c33a7dbd368
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-06-22 18:36:01 +0900

    [Silver IV] Title: 문서 검색, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String target = br.readLine();
+
+        int ans = 0;
+
+        for (int i = 0; i <= s.length() - target.length(); i++) {
+            String sub = s.substring(i, i + target.length());
+            if (sub.equals(target)) {
+                ans++;
+                i += target.length() - 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
