3c69c5536be06160940251e556ff4b188ff33f2c
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-08-11 17:55:48 +0900

    [Bronze III] Title: 홀수, Time: 136 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int min = 100;
+        for (int i = 0; i < 7; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n % 2 == 1) {
+                sum += n;
+                min = Math.min(min, n);
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.printf("%d\n%d", sum, min);
+        }
+    }
+}
