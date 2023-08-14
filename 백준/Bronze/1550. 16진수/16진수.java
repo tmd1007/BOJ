4a051e717523868c3f001102863f9b1aa0e1e481
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-08-14 17:58:11 +0900

    [Bronze II] Title: 16진수, Time: 136 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        int n = Integer.valueOf(s, 16);
+
+        System.out.printf("%d", n);
+
+
+    }
+}
