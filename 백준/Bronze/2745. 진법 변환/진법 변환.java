363c9f976957bc4f2d3d234150d87afaa4505c2a
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-08-17 15:24:51 +0900

    [Bronze II] Title: 진법 변환, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String s = st.nextToken();
+        int N = Integer.parseInt(st.nextToken());
+
+        int ans = Integer.valueOf(s, N);
+        System.out.println(ans);
+    }
+}
