df404426377ec0b7e07dde0ef7273804b62ce2f8
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-08-21 00:17:08 +0900

    [Bronze III] Title: 영수증, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 9; i++) {
+            sum -= Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum);
+    }
+}
