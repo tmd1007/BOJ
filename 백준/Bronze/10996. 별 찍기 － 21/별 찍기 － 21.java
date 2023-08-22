d3609b17924539689ca8c4893aab4d36bb3b66ff
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-08-22 09:13:13 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 136 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N*2; i++) {
+            for (int j = 0; j < N; j++) {
+                if ((i+j) % 2 == 0) {
+                    sb.append('*');
+                } else {
+                    sb.append(' ');
+                }
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
