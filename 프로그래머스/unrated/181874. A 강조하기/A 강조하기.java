64e984fab8389dc0badcfbebfd6fd8ea7d24d0c5
tmd1007 <124874118+tmd1007@users.noreply.github.com>
2023-11-15 16:37:12 +0900

    [level unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,8 @@
 class Solution {
     public String solution(String myString) {
-        char[] arr = myString.toCharArray();
+        myString = myString.toLowerCase();
+        myString = myString.replace('a', 'A');
         
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == 'a') {
-                arr[i] = 'A';
-            } else if (arr[i] != 'A' && Character.isUpperCase(arr[i])) {
-                arr[i] = Character.toLowerCase(arr[i]);
-            }
-        }
-        
-        return String.valueOf(arr);
+        return myString;
     }
 }
